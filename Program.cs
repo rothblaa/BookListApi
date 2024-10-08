65aabac5380756309074c5adfcc15bf5a030d61e
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 03:36:52 -0500

    added the use of CORS

--- Program.cs
+++ Program.cs
@@ -9,7 +9,19 @@ namespace BookApi
             // add services
             builder.Services.AddControllers();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:55185")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("MyCors");
 
             // add mapping
             app.MapControllers(); 
